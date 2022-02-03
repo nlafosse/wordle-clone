7a9c733d0324f5cf2b3731f1b3cc014a9e05d6cd
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-02-03 13:04:34 -0500

    gameboard created

--- app.js
+++ app.js
@@ -29,5 +29,43 @@ const keys = [
   "B",
   "N",
   "M",
-  0xe2,
+  "☠",
 ];
+
+const guessRows = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
+
+  tileDisplay.append(rowElement);
+});
+
+const handleClick = () => {
+  console.log("clicked");
+};
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.setAttribute("id", key);
+  buttonElement.addEventListener("click", handleClick);
+  keyboard.append(buttonElement);
+});
